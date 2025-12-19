52b61a6a647a372c90b8f0d1e8969b69ebd09d9e
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-12-19 18:58:25 +0530

    ts

--- react6/src/index.js
+++ react6/src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom/client'
+import App from './App'
+let root=ReactDOM.createRoot(document.getElementById('root'))
+//root.render("Hello,!GE")         //str
+//root.render(<h2>Hello,!GE</h2>) //tag
+//root.render(<h1>GM</h1><h2>GE</h2>) 
+//root.render(<><h1>GM</h1><h2>GE</h2><h3>GN</h3></>);  //empty JSX fragment
+root.render(<App/>)  //component
